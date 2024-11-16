60b865a4abb069469bc4480a20a6f8e95c8a4258
Yamna Ismail <yamnaisrailkhan@gmail.com>
2024-11-16 09:19:17 +0500

    organized screens in directories

--- lib/screens/settings/show_hide_option/show_hide_option.dart
+++ lib/screens/settings/show_hide_option/show_hide_option.dart
@@ -1,6 +1,9 @@
 import 'package:calender_app/screens/settings/item.dart';
+import 'package:calender_app/screens/settings/show_hide_option/show_hide_symptoms.dart';
 import 'package:calender_app/widgets/backgroundcontainer.dart';
 import 'package:flutter/material.dart';
+
+import 'show_hide_mood.dart';
 
 class ShowHideOption extends StatelessWidget {
   @override
@@ -30,7 +33,13 @@ class ShowHideOption extends StatelessWidget {
                     fontSize: 16
                 ),
               ),
-              trailing: IconButton(onPressed: (){},
+              trailing: IconButton(onPressed: () {
+            Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ShowHideSymptomScreen()
+            ),
+            );
+            },
                   icon: Icon(Icons.arrow_forward_ios)
               )
             ),
@@ -45,7 +54,13 @@ class ShowHideOption extends StatelessWidget {
                             fontSize: 16
                         ),
                       ),
-                      trailing: IconButton(onPressed: (){},
+                      trailing: IconButton(onPressed: (){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ShowHideMood()
+                          ),
+                        );
+                      },
                           icon: Icon(Icons.arrow_forward_ios)
                       )
                   ),
