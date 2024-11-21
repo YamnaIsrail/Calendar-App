354c3ccb9a317246542303c429f860ecd90ee331
Yamna Ismail <yamnaisrailkhan@gmail.com>
2024-11-21 14:31:54 +0500

    Add NotificationService for handling instant and scheduled notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:calender_app/notifications/notification_service.dart';
 import 'package:calender_app/provider/app_state_provider.dart';
 import 'package:calender_app/provider/cycle_provider.dart';
 import 'package:calender_app/provider/intercourse_provider.dart';
@@ -8,36 +9,21 @@ import 'package:calender_app/screens/globals.dart';
 import 'package:calender_app/screens/homeScreen.dart';
 import 'package:calender_app/screens/splash.dart';
 import 'package:flutter/material.dart';
-// import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:menstrual_cycle_widget/menstrual_cycle_widget_base.dart';
-
 import 'hive/notes_model.dart';
-//
-// void main() {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   MenstrualCycleWidget.init(
-//     secretKey: "11a1215l0119a140409p0919",
-//     ivKey: "23a1dfr5lyhd9a1404845001",
-//   );
-//   initializeDateFormatting().then((_) =>
-//       runApp(CalenderApp())
-//   );
-// }
+
+import 'package:timezone/data/latest.dart' as tz;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await NotificationService.init();
+  tz.initializeTimeZones();
+
   await Hive.initFlutter();
   Hive.registerAdapter(NoteAdapter());
   await Hive.openBox<Note>('notesBox');
 
-  // WidgetsFlutterBinding.ensureInitialized();
-  // MenstrualCycleWidget.init(
-  //     secretKey: "11a1215l0119a140409p0919",
-  //     ivKey: "23a1dfr5lyhd9a1404845001"
-  // );
   runApp(
     MultiProvider(
       providers: [
@@ -45,10 +31,9 @@ void main() async {
         ChangeNotifierProvider(create: (_) => PregnancyProvider()),
         ChangeNotifierProvider(create: (_) => AppStateProvider()),
         ChangeNotifierProvider(create: (_) => IntercourseProvider()),
-
         ChangeNotifierProvider(create: (_) => NoteProvider()),
       ],
-      child: CalenderApp(), // `child` is required here
+      child: CalenderApp(),
     ),
   );
 
---
+++ lib/notifications/notification_service.dart
@@ -0,0 +1,65 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+class NotificationService {
+  //Initialization FlutterLocalNotificationsPlugin instance
+  static final FlutterLocalNotificationsPlugin flutterLocalNotification =
+      FlutterLocalNotificationsPlugin();
+
+  //
+  static Future<void> onDidRecieveNotification(
+      NotificationResponse notificationResponse) async {}
+
+//Initialization notification plugin
+  static Future<void> init() async {
+    //for android
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    //for ios
+    const DarwinInitializationSettings initializationSettingsIos =
+        DarwinInitializationSettings();
+    //combine android+ios intialization
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsIos);
+    //Initialize the plugin with specified settings
+    await flutterLocalNotification.initialize(initializationSettings,
+        onDidReceiveBackgroundNotificationResponse: onDidRecieveNotification,
+        onDidReceiveNotificationResponse: onDidRecieveNotification);
+
+//Request notifications for android
+    await flutterLocalNotification
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestNotificationsPermission();
+  }
+
+  //Show an instant Notification
+  static Future<void> showInstantNotification(
+      String title, String body) async {
+    //define notification details
+    const NotificationDetails platformChannels= NotificationDetails(
+      android: AndroidNotificationDetails("channelId", "channelName", importance: Importance.high, priority: Priority.high),
+      iOS: DarwinNotificationDetails()
+    );
+    await flutterLocalNotification.show(0, title, body, platformChannels);
+  }
+
+  //Show a schedule notification
+  static Future<void> showIScheduleNotification(
+      String title, String body, DateTime  scheduleDate) async {
+    //define notification details
+    const NotificationDetails platformChannels= NotificationDetails(
+        android: AndroidNotificationDetails("channelId", "channelName", importance: Importance.high, priority: Priority.high),
+        iOS: DarwinNotificationDetails()
+    );
+    await flutterLocalNotification.zonedSchedule(0, title, body,
+        tz.TZDateTime.from(scheduleDate, tz.local),
+        platformChannels,
+        uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation
+            .absoluteTime,
+      androidScheduleMode: AndroidScheduleMode.exact,
+      matchDateTimeComponents: DateTimeComponents.dateAndTime
+    );
+  }
+}
