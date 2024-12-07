7a1a9a08c30d44b7890e2ecc2ced15927e0cf04f
Yamna Ismail <yamnaisrailkhan@gmail.com>
2024-12-07 12:43:34 +0500

    Integrate url_launcher package for handling external URL navigation

--- lib/screens/flow2/detail page/cycle/ovulation_screen.dart
+++ lib/screens/flow2/detail page/cycle/ovulation_screen.dart
@@ -3,8 +3,12 @@ import 'package:calender_app/screens/globals.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../widgets/buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class OvulationScreen extends StatelessWidget {
+  final String url = 'https://en.wikipedia.org/wiki/Luteinizing_hormone';
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -13,35 +17,6 @@ class OvulationScreen extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Text.rich(
-          //   TextSpan(
-          //     children: [
-          //       TextSpan(
-          //         text: 'A urinary ovulation predictor detects the surge of luteinizing '
-          //             'hormone (LH), indicating the approach of ovulation. It is a test '
-          //             'that measures LH levels in urine, typically occurring 24 to 36 hours '
-          //             'before ovulation, helping identify the most fertile days in a woman’s '
-          //             'cycle for conception.\n\n'
-          //             'For more detail, please check the link below:\n',
-          //         style: TextStyle(fontSize: 16),
-          //       ),
-          //       TextSpan(
-          //         text: 'https://en.wikipedia.org/wiki/Luteinizing_hormone',
-          //         style: TextStyle(color: Colors.blue, fontSize: 16),
-          //         recognizer: TapGestureRecognizer()
-          //           ..onTap = () async {
-          //             // Launch the URL when the text is tapped
-          //             final url = 'https://en.wikipedia.org/wiki/Luteinizing_hormone';
-          //             if (await canLaunch(url)) {
-          //               await launch(url);
-          //             } else {
-          //               throw 'Could not launch $url';
-          //             }
-          //           },
-          //       ),
-          //     ],
-          //   ),
-          // ),
           Text(
             'LH Test',
             style: TextStyle(
@@ -57,14 +32,32 @@ class OvulationScreen extends StatelessWidget {
                 'that measures LH levels in urine, typically occurring 24 to 36 hours '
                 'before ovulation, helping identify the most fertile days in a woman’s '
                 'cycle for conception.\n\n'
-                'For more detail, please check the link below:\n'
-                'https://en.wikipedia.org/wiki/Luteinizing_hormone',
+                'For more detail, please check the link below:',
             style: TextStyle(fontSize: 16),
+          ),
+          SizedBox(height: 8),
+          GestureDetector(
+            onTap: () async {
+              if (await canLaunchUrl(Uri.parse(url))) {
+                await launchUrl(Uri.parse(url));
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Could not launch the URL')),
+                );
+              }
+            },
+            child: Text(
+              url,
+              style: TextStyle(
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
+              ),
+            ),
           ),
           SizedBox(height: 20),
           CustomButton(
             text: 'OK',
-            onPressed: ()  {
+            onPressed: () {
               Navigator.pop(context);
             },
             backgroundColor: primaryColor,
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,6 +16,7 @@ import local_auth_darwin
 import path_provider_foundation
 import share_plus
 import sqflite_darwin
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
@@ -29,4 +30,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
